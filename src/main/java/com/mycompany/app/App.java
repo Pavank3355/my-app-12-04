c515c3db26d94ce770551d77b6ddc22b70274a5b
Pavank3355 <130123253+Pavank3355@users.noreply.github.com>
2023-04-13 17:07:22 +0530

    Added Jenkins

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from AWS and MAven!" );
+        System.out.println( "Hello World from AWS and Maven and Jenkins!" );
     }
 }
